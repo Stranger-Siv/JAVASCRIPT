6f4721a0de3fe3a6a88269327b1a8dcec4fbfa0a
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-03 06:36:41 +0000

    container config done

--- 01_BASICS/01_variables.js
+++ 01_BASICS/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 210305105516
 let accountEmail = "Programmersiv21@gmail.com"
 var accountPassword = "12345"
 accountCity = "Vadodara"
+let accountState
 
 // accountId = 2    Not allowed
 console.log(accountId)
@@ -10,4 +11,8 @@ accountEmail = "abc12@gmail.com"
 accountPassword = "2341"
 accountCity = "Surat"
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
