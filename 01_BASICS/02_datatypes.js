edc67d710281eb431caf2893cd6978796ec10c11
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-03 06:51:12 +0000

    datatypes

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello"). we are using node js not browser
+
+console.log(3+3)
+console.log("Sivram")
+
+// number => 2 to power 53
+// bigint
+// string => "Hello"
+// boolean => true/false
+// null => standlone value
+// undefined => value not assigned
+// symbol => unique
+
+console.log(typeof"Sivram") //string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof 123) // number
+console.log(typeof true) // boolean
