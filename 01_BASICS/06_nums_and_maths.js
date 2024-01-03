5145c3d83cc887129c44b539cbeb57e15bd355e6
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-03 11:32:17 +0000

    nums and math

--- 01_BASICS/06_nums_and_maths.js
+++ 01_BASICS/06_nums_and_maths.js
@@ -1,1 +1,30 @@
-console.log("Hello")
+const score =400
+const balance =new Number(100)
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(2));
+
+const otherNumber = 26.2567
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+// +++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+console.log(Math.min(4,5,6,7));
+console.log(Math.max(4,5,6,7,8))
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*10)+min)
