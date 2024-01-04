a13322bceb4fa82ae805cce0f171d4e1f7b3c677
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-04 04:53:01 +0000

    more on arrays

---
+++ 02_BASICS/02_arrays.js
@@ -0,0 +1,27 @@
+const marvel_heros = ["thor","captain","ironman"]
+const dc_heros = ["superman","batman","flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// const allNewHeros = [...dc_heros,...marvel_heros]
+// console.log(allNewHeros);
+
+// const anotherArr = [1,2,3[4,5,6],7,[6,7[4,5]]]
+// const anotherReal = anotherArr.flat(Infinity)
+// console.log(anotherReal);
+
+// console.log(Array.isArIay ("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
