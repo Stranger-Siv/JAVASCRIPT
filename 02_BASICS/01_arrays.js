1bd0e17a775c2ca92f47cfc5dbaf2aa4063b1f19
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-04 04:22:41 +0000

    array

--- 02_BASICS/01_arrays.js
+++ 02_BASICS/01_arrays.js
@@ -1,1 +1,35 @@
-console.log("Here we will study about arrays as data structure")
+const array =[1,2,3,4,5]
+const heros = ["Ironman","Shaktiman","Sivram"]
+
+const myArray = new Array(1,2,3,4,5,6,7)
+// console.log(myArray[1]);
+
+// myArray.push(6)
+// myArray.push(7)
+// console.log(myArray);
+
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9))
+
+// const newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// spice and slice
+
+console.log("A",myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B",myArray);
+
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+console.log("C",myArray);
