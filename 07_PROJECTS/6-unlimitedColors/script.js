82903e64cd856582e9fd377d824b42a641f8c910
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-08 16:43:06 +0000

    project 6

---
+++ 07_PROJECTS/6-unlimitedColors/script.js
@@ -0,0 +1,33 @@
+// Generte a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+let intervalId;
+
+let startChangingColor = function(){
+    if(!intervalId){
+        intervalId=setInterval(changeBGColor,100)
+    }
+    
+
+    function changeBGColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+
+let stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId=null;
+}
+document.querySelector('#start').
+addEventListener('click',startChangingColor)
+
+document.querySelector('#stop').
+addEventListener('click',stopChangingColor)
