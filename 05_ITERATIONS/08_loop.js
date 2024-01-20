f9ee78ae8a1241ebfbd0f547b6f561c751cea9ef
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-20 15:34:14 +0000

    loop 8

--- 05_ITERATIONS/08_loop.js
+++ 05_ITERATIONS/08_loop.js
@@ -4,7 +4,7 @@ const myNums = [1,2,3]
 //     console.log(`acc:${acc} and currval: ${currval} `);
 //     return acc + currval
 // },0)
-
+// Here we will study on arrow function
 const myTotal = myNums.reduce((acc,currval)=>acc+currval,0)
 // console.log(myTotal);
 
