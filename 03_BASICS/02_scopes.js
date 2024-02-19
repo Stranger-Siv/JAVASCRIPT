33ed6b85fc8954e05b3ece4805b2d0f851fc8a9a
Sivram Pradhan <programmersiv21@gmail.com>
2024-02-19 20:13:23 +0530

    Update 02_scopes.js

--- 03_BASICS/02_scopes.js
+++ 03_BASICS/02_scopes.js
@@ -6,7 +6,6 @@ if(true){
     // var c=30 // it is also applicable outside the scope so it makes confusion so we do not use var as a datatype in js
     // console.log("INNER:",a)
 }
-
 // console.log(a);
 // console.log(b);
 // console.log(c);
@@ -39,9 +38,6 @@ console.log(addone(5));
 function addone(num){
     return num+1
 }
-
-
-
 // console.log(addTwo(5))  // in this way we cannot call function before initialization
 const addTwo = function (num){
     return num+2
