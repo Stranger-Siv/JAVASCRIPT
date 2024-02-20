c9ff52d7f37a2185cfa017604deb3569fc4d31fc
Sivram Pradhan <programmersiv21@gmail.com>
2024-02-20 17:23:05 +0530

    Update 01_loop.js

--- 05_ITERATIONS/01_loop.js
+++ 05_ITERATIONS/01_loop.js
@@ -6,11 +6,8 @@ for (let i = 0; i <= 10; i++) {
         //console.log("5 is best number");
     }
     //console.log(element);
-    
 }
-
 // console.log(element);
-
 for (let i = 1; i <= 10; i++) {
     //console.log(`Outer loop value: ${i}`);
    for (let j = 1; j <= 10; j++) {
